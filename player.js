0a18220b4ee6497893a943b275539e448271aa98
Sleezzi <93204760+Sleezzi@users.noreply.github.com>
2023-01-06 10:59:56 +0100

    Update player.js

--- player.js
+++ player.js
@@ -1,203 +1,218 @@
-const main = document.getElementById("main");
-const navTwitch = document.getElementById("twitchNav");
-const navYoutube = document.getElementById("youtubeNav");
-const navSettings = document.getElementById("settingsNav");
-const twitchContent = document.getElementById("twitchContent");
-const youtubeContent = document.getElementById("youtubeContent");
-const settingsContent = document.getElementById("settingsContent");
+console.log("%cYou're not supposed to see this", 'margin: 0; color: white; background: black; font-size: 50px; text-align: center; padding-top: calc(100% * 100%);');
 
-const notificationTwitchSwitchContener = document.getElementById("notificationTwitchSwitchContener");
-const notificationTwitchSwitchButton = document.getElementById("notificationTwitchSwitchButton");
-const notificationTwitchText = document.getElementById("notificationTwitchText");
-const notificationYoutubeSwitchContener = document.getElementById("notificationYoutubeSwitchContener");
-const notificationYoutubeSwitchButton = document.getElementById("notificationYoutubeSwitchButton");
-const notificationYoutubeText = document.getElementById("notificationYoutubeText");
-const theme = document.getElementById("menu");
-const audio = document.getElementById("audio");
-audio.volume = 1;
+const main = document.getElementById('main');
+const menu = document.getElementById('menu');
+const audio = document.getElementById('audio');
+const start = document.getElementById("Play");
+const player = document.getElementById("player")
+const audioSelect = document.getElementById("audioSelect");
+const progressRange = document.getElementById("progress-range");
+const progressbar = document.getElementById("progress-bar");
+const progressValue = document.getElementById("progressValue");
+const volume = document.getElementById("volume");
+const volumeImg = document.getElementById("volume-img");
+const volumeRange = document.getElementById("volume-range");
+const volumeValue = document.getElementById("volume-value");
+const volumeProgress = document.getElementById("progress-volume");
+const loop = document.getElementById("loop");
+const loopImg = document.getElementById("loop-img");
 
-if (localStorage.getItem("notifiactionTwitch") != "undefined" | localStorage.getItem("notifiactionTwitch") != null) {
-  if (localStorage.getItem("notifiactionTwitch") === "false") {
-    notificationTwitchSwitchContener.style.backgroundColor = "red";
-    notificationTwitchSwitchButton.style.marginLeft = "-18px";
-  } else {
-    notificationTwitchSwitchContener.style.backgroundColor = "green";
-    notificationTwitchSwitchButton.style.marginLeft = "calc(50% - 3px)";
-  }
-} else  {
-  notificationTwitchSwitchContener.style.backgroundColor = "green";
-  notificationTwitchSwitchButton.style.marginLeft = "calc(50% - 3px)";
-  localStorage.setItem("notifiactionTwitch", "true");
-};
+let sec = 0
+let min = 0
 
-if (localStorage.getItem("notifiactionYoutube") != "undefined" | localStorage.getItem("notifiactionYoutube") != null) {
-  if (localStorage.getItem("notifiactionYoutube") === "false") {
-    notificationYoutubeSwitchContener.style.backgroundColor = "red";
-    notificationYoutubeSwitchButton.style.marginLeft = "-18px";
-  } else {
-    notificationYoutubeSwitchContener.style.backgroundColor = "green";
-    notificationYoutubeSwitchButton.style.marginLeft = "calc(50% - 3px)";
-  }
-} else  {
-  notificationYoutubeSwitchContener.style.backgroundColor = "green";
-  notificationYoutubeSwitchButton.style.marginLeft = "calc(50% - 3px)";
-  localStorage.setItem("notifiactionYoutube", true);
-};
+let theme = "white";
 
-if (localStorage.getItem("theme") != "undefined" | localStorage.getItem("theme") != null) {
-  theme.value = localStorage.getItem("theme");
-  if (localStorage.getItem("theme") === "System") {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    if (darkModeMediaQuery.matches) {
-      main.classList.add('dark-mode');
+let isPlaying = false;
+let isLoop = false;
+
+if (sessionStorage.getItem("audioPlayed") === null || sessionStorage.getItem("audioPlayed") === 'undefined') {
+    sessionStorage.setItem("progress", 0);
+    if (sessionStorage.getItem("progress") === null || sessionStorage.getItem("progress") === 'undefined') {
+        sessionStorage.setItem("progress", 0);
     } else {
-      main.classList.remove('dark-mode');
+        progressRange.value = sessionStorage.getItem("progress");
+        progressValue.innerHTML = parseInt(sessionStorage.getItem("progress")) + "s";
+        progressbar.value = sessionStorage.getItem("progress");
+        audio.currentTime = sessionStorage.getItem("progress");
     }
-  } else if (localStorage.getItem("theme") === "Dark") {
-    main.classList.add('dark-mode');
-  } else if (localStorage.getItem("theme") === "Light") {
-    main.classList.remove('dark-mode');
-  } else {
-    const date = new Date();
-    const hour = date.getHours();
-    if (hour >= 18 || hour < 6) {
-      main.classList.add('dark-mode');
-    } else {
-      main.classList.remove('dark-mode');
-    }
-  }
-} else  {
-  theme.value = "Dark";
-  main.classList.add('dark-mode');
-  
-  localStorage.setItem("theme", "Dark");
-};
-
-function nav(Button, Page) {
-  navTwitch.style.filter = "none";
-  navTwitch.style.borderRadius = "100vh";
-  twitchContent.style.display = "none";
-
-  navYoutube.style.filter = "none";
-  navYoutube.style.borderRadius = "100vh";
-  youtubeContent.style.display = "none";
-
-  navSettings.style.filter = "none";
-  navSettings.style.borderRadius = "100vh";
-  settingsContent.style.display = "none";
-
-  Button.style.borderRadius = "15px";
-  Button.style.filter = "invert()";
-  Page.style.display = "block";
-};
-
-navTwitch.onmouseup = function () {nav(navTwitch, twitchContent)};
-navYoutube.onmouseup = function () {nav(navYoutube, youtubeContent)};
-navSettings.onmouseup = function () {nav(navSettings, settingsContent)};
-
-if (window.location.href.indexOf("options") > -1) {
-  nav(navSettings, settingsContent);  
-}
-
-theme.addEventListener('change', event => {
-  const value = event.target.value;
-  localStorage.setItem("theme", value);
-  if (value === 'Dark') {
-    main.classList.add('dark-mode');
-  } else if (value === 'Light') {
-    main.classList.remove('dark-mode');
-  } else if (value === 'Hours') {
-    const date = new Date();
-    const hour = date.getHours();
-    if (hour >= 18 || hour < 6) {
-      main.classList.add('dark-mode');
-    } else {
-      main.classList.remove('dark-mode');
-    }
-  } else {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    if (darkModeMediaQuery.matches) {
-      main.classList.add('dark-mode');
-    } else {
-      main.classList.remove('dark-mode');
-    }
-  }
-});
-
-function notification(Button, Contener, Storage) {
-  if (localStorage.getItem(Storage) === "true") {
-    Contener.style.backgroundColor = "red";
-    Button.style.marginLeft = "calc(0% - 18px)";
-    Button.border = "",
-    localStorage.setItem(Storage, "false");
-  } else {
-    Contener.style.backgroundColor = "green";
-    Button.style.marginLeft = "calc(50% - 3px)";
-    localStorage.setItem(Storage, "true");
-    audio.play();
-  }
-};
-
-notificationTwitchSwitchContener.onmouseup = function() {notification(notificationTwitchSwitchButton, notificationTwitchSwitchContener, "notifiactionTwitch")};
-notificationTwitchText.onmouseup = function() {notification(notificationTwitchSwitchButton, notificationTwitchSwitchContener, "notifiactionTwitch")};
-
-notificationYoutubeSwitchContener.onmouseup = function() {notification(notificationYoutubeSwitchButton, notificationYoutubeSwitchContener, "notifiactionYoutube")};
-notificationYoutubeText.onmouseup = function() {notification(notificationYoutubeSwitchButton, notificationYoutubeSwitchContener, "notifiactionYoutube")};
-
-function VideoFetch() {
-  fetch("https://sleezzi.github.io/extension/YouTubeVideo.json", {
-    method: "GET"
-  }).then(response => {return response.json()}).then(data => {
-    if (!data.error) {
-      const MyLastVideo = document.getElementById("LastVideo");
-      const MyLastVideoImg = document.getElementById("LastVideoImage");
-      const MyLastVideoTitle = document.getElementById("LastVideoTitle");
-      const MyLastVideoPublishedAt = document.getElementById("LastVideoPublishedAt");
-      const MyLastVideoDuration = document.getElementById("LastVideoDuration");
-      
-      MyLastVideo.setAttribute("href", "https://youtube.com/watch?v=" + data.items[0].id + "&t=0");
-      MyLastVideo.setAttribute("alt", data.items[0].title)
-      MyLastVideo.style.cursor = "pointer";
-      MyLastVideoTitle.innerHTML = data.items[0].title;
-      MyLastVideoImg.style.background = 'url("https://i.ytimg.com/vi/' + data.items[0].id + '/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGDuO3MAuK81v9cn7N6t5rGAFIBA") no-repeat center';
-      MyLastVideoImg.style.backgroundSize = "cover";
-      const duration = data.items[0].duration;
-      const timeParts = duration.match(/([0-9]+)([HMS])/g)
-
-      if (parseInt(timeParts[2]) > 0) { // Heure + Minute + Seconde
-        MyLastVideoDuration.innerHTML = "Durée : " + parseInt(timeParts[0]) + ":" + (parseInt(timeParts[1]) < 10 ? "0" + parseInt(timeParts[1]) : parseInt(timeParts[1])) + ":" + (parseInt(timeParts[2]) > 0 ? (parseInt(timeParts[2]) < 10 ? "0" + parseInt(timeParts[2]) : parseInt(timeParts[2])) : "00");
-      } else if (parseInt(timeParts[1]) > 0) { // Minute + Seconde
-        MyLastVideoDuration.innerHTML = "Durée : " + parseInt(timeParts[0]) + ":" + (parseInt(timeParts[1]) > 0 ? (parseInt(timeParts[1]) < 10 ? "0" + parseInt(timeParts[1]) : parseInt(timeParts[1])) : "00");
-      } else { // Seconde
-        MyLastVideoDuration.innerHTML = "Durée : " + parseInt(timeParts[0]) + "s";
-      }
-
-      const date = new Date(data.items[0].publishedAt).getTime() / 1000;
-  
-      var diff = Math.floor(Date.now() / 1000) - date;
-    
-      if (diff < 60) {
-        return MyLastVideoPublishedAt.innerHTML = "Il y a quelque secondes.";
-      } else if (diff < 3600) {
-        var amount = Math.floor(diff / 60);
-        return MyLastVideoPublishedAt.innerHTML = "Il y a " + amount + " minute" + (amount > 1 ? "s" : "");
-      } else if (diff < 86400) {
-        var amount = Math.floor(diff / 3600);
-        return MyLastVideoPublishedAt.innerHTML = "Il y a " + amount + " heure" + (amount > 1 ? "s" : "");
-      } else if (diff < 2419200) {
-        var amount = Math.floor(diff / 86400);
-        return MyLastVideoPublishedAt.innerHTML = "Il y a " + amount + " jour" + (amount > 1 ? "s" : "");
-      } else if (diff < 29030400) {
-        return MyLastVideoPublishedAt.innerHTML = Math.floor(diff / 2419200) + " mois";
-      } else {
-        var amount = Math.floor(diff / 29030400);
-        return MyLastVideoPublishedAt.innerHTML = "Il y a " + amount + " an" + (amount > 1 ? "s" : "");
-      }
-    }    
-  });
-};
-if (navigator.onLine) {
-  setInterval(function() {VideoFetch()}, 60);
 } else {
-  console.log("Connectez-vous à Internet pour profiter de toute les fonctionnalité de cette extension");
-}
+    audio.setAttribute("src", sessionStorage.getItem("audioPlayed"));
+    player.style.display = "flex";
+    progressbar.max = sessionStorage.getItem("audioDuration");
+    progressValue.max = sessionStorage.getItem("audioDuration");
+};
+
+if (localStorage.getItem("volume") === null || localStorage.getItem("volume") === 'undefined') {
+    localStorage.setItem("volume", 1);
+} else {
+    volumeRange.value = localStorage.getItem("volume") * 100;
+    audio.volume = localStorage.getItem("volume");
+    volumeValue.innerHTML = (parseInt(localStorage.getItem("volume") * 100) < 10 ? "0" + parseInt(localStorage.getItem("volume") * 100) : parseInt(localStorage.getItem("volume") * 100)) + "%";
+};
+
+if (localStorage.getItem("loop") === null || localStorage.getItem("loop") === 'undefined') {
+    localStorage.setItem("loop", false);
+    audio.loop = false;
+    loopImg.setAttribute("src", "https://sleezzi.github.io/img/loopOff.png");
+} else {
+    if (localStorage.getItem("loop") === "true") {
+        audio.loop = true;
+        loopImg.setAttribute("src", "https://sleezzi.github.io/img/loopOn.png");
+    } else {
+        audio.loop = false;
+        loopImg.setAttribute("src", "https://sleezzi.github.io/img/loopOff.png");
+    }
+};
+
+audioSelect.onchange = function() {
+    const file = audioSelect.files[0];
+    let fileReader = new FileReader();
+        fileReader.onloadend = function(event) {
+            audio.setAttribute("src", event.target.result);
+            player.style.display = "flex";
+            start.innerHTML = "Play";
+            isPlaying = false;
+        };
+    fileReader.readAsDataURL(document.getElementById("audioSelect").files[0]);
+    document.getElementById("title").innerHTML = "Player | " + file.name;
+};
+
+progressValue.style.textAlign = "center";
+progressValue.style.color = "white";
+progressbar.style.width = "200px";
+progressbar.style.height = "20px";
+progressbar.style.webkitAppearance = "none";
+progressbar.style.position = "absolute";
+progressbar.style.top = "0px";
+progressbar.style.borderRadius = "10px";
+progressbar.style.overflow = "hidden";
+progressbar.style.alignContent = "center";
+
+start.onclick = function() {
+    if (isPlaying === false) {
+        isPlaying = true;  
+        audio.volume = localStorage.getItem("volume");
+        audio.play();
+        start.innerHTML = "Pause";
+        progressRange.setAttribute("max", audio.duration);
+        progressbar.setAttribute("max", audio.duration);
+        sessionStorage.setItem("audioDuration", audio.duration);
+    } else {
+        isPlaying = false;  
+        audio.pause();
+        start.innerHTML = "Play";
+        progressbar.value = audio.currentTime;
+        progressValue.innerHTML = parseInt(audio.currentTime.toFixed(0));
+        progressRange.value = audio.currentTime;
+    }
+};
+
+audio.ontimeupdate = function() {
+    progressbar.value = audio.currentTime;
+    progressRange.value = audio.currentTime;
+    progressValue.style.color = "white";
+    progressbar.style.width = "200px";
+    progressbar.style.height = "20px";
+    progressbar.style.webkitAppearance = "none";
+    progressbar.style.position = "absolute";
+    progressbar.style.top = "0px";
+    progressbar.style.left = "0px;"
+    progressbar.style.borderRadius = "10px";
+    progressbar.style.overflow = "hidden";
+    progressbar.style.alignContent = "center";
+    sessionStorage.setItem("progress", audio.currentTime);
+    sec = audio.currentTime.toFixed(0);
+    min = audio.currentTime.toFixed(0) / 60;
+    progressValue.innerHTML = (min >= 1 ? parseInt(min) + (sec < 10 ? ":0 "+ parseInt(sec) : ":" + parseInt(sec)) : (sec < 10 ? progressValue.innerHTML = "0" + parseInt(sec): progressValue.innerHTML = parseInt(sec)) + "s");
+};
+
+audio.onended = function() {
+    if (isLoop != true) {
+        start.innerHTML = "Play";
+        isPlaying = "false";
+        progressbar.value = 0;
+        progressValue.innerHTML = 0;
+        progressRange.value = 0;
+        audio.currentTime = 0;
+    }
+};
+
+progressRange.oninput = function() {
+    progressbar.value = this.value;
+    progressValue.innerHTML = (min >= 1 ? parseInt(min) + (sec < 10 ? ":0 "+ parseInt(sec) : ":" + parseInt(sec)) : (audio.currentTime.toFixed(0) < 10 ? progressValue.innerHTML = "0" + parseInt(sec) + "s": progressValue.innerHTML = parseInt(sec) + "s"));
+    audio.currentTime = this.value;
+    progressValue.style.textAlign = "center";
+    progressValue.style.color = "white";
+    progressbar.style.width = "200px";
+    progressbar.style.height = "20px";
+    progressbar.style.webkitAppearance = "none";
+    progressbar.style.position = "absolute";
+    progressbar.style.top = "0px";
+    progressbar.style.borderRadius = "10px";
+    progressbar.style.overflow = "hidden";
+    progressbar.style.alignContent = "center";
+    this.setAttribute("max", audio.duration);
+    progressbar.setAttribute("max", audio.duration);
+    sessionStorage.setItem("audioDuration", audio.duration);
+    audio.pause()
+};
+
+progressRange.onclick = function() {
+    if (isPlaying === true) {
+        audio.play()
+    }
+};
+
+volume.onmousemove = function() {
+    volumeImg.style.display = "flex";
+    volumeRange.style.display = "flex";
+    volumeRange.style.marginLeft = "15px";
+    volumeValue.style.display = "flex";
+    volumeValue.style.fontFamily = "'fantasy'";
+};
+
+volume.onmouseleave = function() {
+    volumeImg.style.display = "flex";
+    volumeRange.style.display = "none";
+    volumeValue.style.display = "none";
+    volumeRange.style.marginLeft = "0";
+};
+
+volumeRange.oninput = function() {
+    audio.volume = this.value / 100;
+    volumeRange.style.marginLeft = "15px";
+    localStorage.setItem("volume", this.value / 100);
+    volumeValue.innerHTML = (this.value < 10 ? "0" + this.value : this.value) + "%";
+    volumeImg.setAttribute("src", (volumeRange.value === 0 ? "https://sleezzi.github.io/img/mute.png" : "https://sleezzi.github.io/img/audio.png"));
+};
+
+volumeImg.onmouseup = function() {
+    if (audio.volume === 0) {
+        audio.volume = localStorage.getItem("volume");
+        volumeImg.setAttribute("src", "https://sleezzi.github.io/img/audio.png");
+        volumeValue.innerHTML = parseInt(localStorage.getItem("volume") * 100) + "%";
+        volumeRange.value = parseInt(localStorage.getItem("volume") * 100);
+    } else {
+        audio.volume = 0;
+        volumeImg.setAttribute("src", "https://sleezzi.github.io/img/mute.png");
+        volumeRange.style.marginLeft = "15px";
+        volumeRange.value = 0;
+        volumeValue.innerHTML = "0%";
+    }
+};
+
+loop.onclick = function() {
+    if (isLoop === true) {
+        isLoop = false;
+        localStorage.setItem("loop", false);
+        audio.loop = false;
+        loopImg.setAttribute("src", "https://sleezzi.github.io/img/loopOff.png");
+    } else {
+        isLoop = true;
+        localStorage.setItem("loop", true);
+        audio.loop = true;
+        loopImg.setAttribute("src", "https://sleezzi.github.io/img/loopOn.png");
+    }
+};
+
+window.addEventListener("error", (event) => console.warn(event));
